5e53e64f46fae07f8d3cbfa3742f2037e0754668
DaanAr <127621104+DaanAr@users.noreply.github.com>
2023-05-02 00:04:54 -0300

    Update index.js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//Menú mobile//
+
+const menuBtn = document.querySelector('.menu-btn')
+const navLinks = document.querySelector ('.nav-links')
+
+menuBtn.addEventListener('click', ()=> {
+    navLinks.classList.toggle('mobile-menu')
+})
