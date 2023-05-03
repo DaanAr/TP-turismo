d27ebf4da0a2749700be1fe641cd33efd5bd45af
DaanAr <127621104+DaanAr@users.noreply.github.com>
2023-05-03 14:25:08 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,3 +6,17 @@ const navLinks = document.querySelector ('.nav-links')
 menuBtn.addEventListener('click', ()=> {
     navLinks.classList.toggle('mobile-menu')
 })
+
+//Slider//
+
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+const slider = document.querySelector('.slider');
+
+prevBtn.addEventListener('click', () => {
+  slider.scrollLeft -= 470;
+});
+
+nextBtn.addEventListener('click', () => {
+  slider.scrollLeft += 470;
+});
