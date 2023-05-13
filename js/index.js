f6f467342d9e23192ac6e2d281f85681ca243f67
DaanAr <127621104+DaanAr@users.noreply.github.com>
2023-05-13 03:40:18 -0300

    index.js

--- js/index.js
+++ js/index.js
@@ -6,17 +6,45 @@ const navLinks = document.querySelector ('.nav-links')
 menuBtn.addEventListener('click', ()=> {
     navLinks.classList.toggle('mobile-menu')
 })
+//SLIDER
 
-//Slider//
+// Acceso a las imágenes
+const slideImages = document.querySelectorAll('#images');
 
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
-const slider = document.querySelector('.slider');
+// Acceso a los botones del DOM
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
 
-prevBtn.addEventListener('click', () => {
-  slider.scrollLeft -= 470;
-});
+// Acceso a los indicadores del DOM
+const indicadores = document.querySelectorAll('.indicador');
 
-nextBtn.addEventListener('click', () => {
-  slider.scrollLeft += 470;
-});
+var contador = 0;
+
+// Código para los botones next y prev
+
+next.addEventListener('click', slideNext);
+prev.addEventListener('click', slidePrev);
+
+function slideNext() {
+  slideImages[contador].style.animation = 'next1 0.5s ease-in forwards';
+  indicadores[contador].classList.remove('active');
+  if (contador >= slideImages.length - 1) {
+    contador = 0;
+  } else {
+    contador++;
+  }
+  slideImages[contador].style.animation = 'next2 0.5s ease-in forwards';
+  indicadores[contador].classList.add('active');
+}
+
+function slidePrev() {
+  slideImages[contador].style.animation = 'prev1 0.5s ease-in forwards';
+  indicadores[contador].classList.remove('active');
+  if (contador <= 0) {
+    contador = slideImages.length - 1;
+  } else {
+    contador--;
+  }
+  slideImages[contador].style.animation = 'prev2 0.5s ease-in forwards';
+  indicadores[contador].classList.add('active');
+}
